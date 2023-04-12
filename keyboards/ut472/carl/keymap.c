fa07b0ffdbe9c82bb94a8586f5f5c976e1c4ea4f
cf <cf@localhost>
2023-04-12 14:35:47 +0200

    update ut472 keymap

--- keyboards/ut472/carl/keymap.c
+++ keyboards/ut472/carl/keymap.c
@@ -31,9 +31,9 @@ enum custom_keycodes {
 
 #define LT3_TAB LT(_L3, KC_TAB)
 #define LT3_MINUS LT(_L3, KC_MINUS)
-#define LT2_TAB LT(_L2, KC_TAB)
-#define LT2_SPC LT(_L2, KC_SPC)
-#define LT1_RET LT(_L1, KC_ENT)
+#define LT1_TAB LT(_L1, KC_TAB)
+//#define LT2_SPC LT(_L2, KC_SPC)
+#define LT2_RET LT(_L2, KC_ENT)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,15 +52,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QW] = LAYOUT(
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    LT3_MINUS,
-  KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, LCTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_SPC,
-  KC_LCTL, KC_LALT, KC_LEFT, KC_RIGHT,  LT2_TAB, KC_SPC,  LT1_RET, KC_UP, KC_DOWN, KC_LGUI,   KC_APP
+  KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RALT_T(KC_QUOT),
+  SC_LSPO, LCTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), SC_RSPC,
+  KC_LCTL, KC_LALT, KC_LEFT, KC_RIGHT,  LT1_TAB, KC_SPC,  LT2_RET, KC_UP, KC_DOWN, KC_LGUI,   KC_APP
 ),
 [_CM] = LAYOUT(
   KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    LT3_MINUS,
-  KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT,
-  KC_LSFT, LCTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_SPC,
-  KC_LCTL, KC_LALT, KC_LEFT, KC_RIGHT,  LT2_TAB, KC_SPC,  LT1_RET, KC_UP, KC_DOWN, KC_LGUI,   KC_APP
+  KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O, RALT_T(KC_QUOT),
+  SC_LSPO, LCTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), SC_RSPC,
+  KC_LCTL, KC_LALT, KC_LEFT, KC_RIGHT,  LT1_TAB, KC_SPC,  LT2_RET, KC_UP, KC_DOWN, KC_LGUI,   KC_APP
 ),
 
   /* FN Layer 1
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_L1] = LAYOUT( /* Right */
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELETE,
-  _______, RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+  KC_DEL , RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
   _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  _______,  _______,  _______,  _______,
   _______, _______, KC_HOME, KC_END, _______,     _______,      _______, KC_PGUP, KC_PGDN, _______, _______
 ),
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_L2] = LAYOUT( /* Left */
   KC_TILDE,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_DELETE,
-  _______, _______, _______, _______, _______, _______, _______, KC_UNDERSCORE, KC_PLUS, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PIPE,
+  KC_DEL , _______, _______, _______, _______, _______, _______, KC_UNDERSCORE, KC_PLUS, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PIPE,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
   _______, _______, KC_HOME, KC_END, _______,     _______,      _______, KC_PGUP, KC_PGDN, _______, _______
 ),

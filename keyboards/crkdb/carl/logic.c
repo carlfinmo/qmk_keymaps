298ef065074dab011c8cc2eafac4f28e74a2c154
cf <cf@localhost>
2023-04-14 20:50:02 +0200

    add bongocat of internet world fame to crkdb
    some config options had to be enabled: wpm and split transport
    and disabled: led stuff, to decrease size

--- keyboards/crkdb/carl/logic.c
+++ keyboards/crkdb/carl/logic.c
@@ -1,3 +1,5 @@
+#include "bongo.c"
+
 void caps_word_set_user(bool active) {
     if (is_keyboard_master()) {
     oled_set_cursor(0, 1);
@@ -37,6 +39,31 @@ bool oled_task_user(void) {
     // needed for bongocat?
     /* oled_render_keylog(); */
   } else {
+    // oled_write_ln_P(PSTR("Uhu?"), false);
+    //  And if you want to put your image on the slave side, put it here
+    //  instead:
+    // my_render_logo();
+    render_anim(); // renders pixelart
+
+    oled_set_cursor(
+        0, 0); // sets cursor to (row, column) using charactar spacing (5 rows
+               // on 128x32 screen, anything more will overflow back to the top)
+    // sprintf(wpm_str, "WPM:%03d", get_current_wpm());  // edit the string to
+    // change wwhat shows up, edit %03d to change how many digits show up
+    // oled_write(wpm_str, false);                       // writes wpm on top
+    // left corner of string
+
+    // This way it's more size efficient than calling sprintf
+    /* oled_write_P(PSTR("WPM: "), false); */
+    /* oled_write(get_u8_str(get_current_wpm(), ' '), false); */
+    // This way it's more size efficient than calling sprintf
+
+    led_t led_state =
+        host_keyboard_led_state(); // caps lock stuff, prints CAPS on new line
+                                   // if caps led is on
+
+    oled_set_cursor(0, 1);
+    oled_write_P(led_state.caps_lock ? PSTR("CAPS") : PSTR("       "), false);
     // render_logo();
   }
   return false;
--- keyboards/crkdb/carl/rules.mk
+++ keyboards/crkdb/carl/rules.mk
@@ -1,7 +1,9 @@
 MOUSEKEY_ENABLE     = no     # Mouse keys
 VIA_ENABLE          = no         # Enable VIA
-#RGBLIGHT_ENABLE = no
-#RGB_MATRIX_ENABLE = yes
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
 COMMAND_ENABLE = no
 
+OLED_DRIVER_ENABLE = yes
+WPM_ENABLE = yes
 CAPS_WORD_ENABLE = yes

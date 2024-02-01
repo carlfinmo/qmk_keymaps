9c95271cd6502e34fddef52f40eda3873594fa28
cf <cf@localhost>
2024-02-01 13:27:13 +0100

    ut472 keymap updates

--- keyboards/ut472/carl/keymap.c
+++ keyboards/ut472/carl/keymap.c
@@ -22,6 +22,8 @@ enum layers {
   _L2,
   _L3,
 };
+#define FIRST_LAYER _QW
+#define LAST_LAYER _L3
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -76,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
 [_L1] = LAYOUT( /* Right */
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELETE,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS,
   KC_DEL , RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
   _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  _______,  _______,  _______,  _______,
   _______, _______, KC_HOME, KC_END, _______,     _______,      _______, KC_PGUP, KC_PGDN, _______, _______
@@ -96,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_L2] = LAYOUT( /* Left */
   KC_TILDE,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_DELETE,
-  KC_DEL , _______, _______, _______, _______, _______, _______, KC_UNDERSCORE, KC_PLUS, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PIPE,
+  KC_DEL , _______, _______, _______, _______, _______, _______, KC_EQL, KC_PLUS, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PIPE,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
   _______, _______, KC_HOME, KC_END, _______,     _______,      _______, KC_PGUP, KC_PGDN, _______, _______
 ),
